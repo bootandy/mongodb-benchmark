0aec327da09eb0b2560ef12a31a6f4cc8cc12d3e
andy boot <bootandy@gmail.com>
2012-10-01 19:54:54 +0100

    refining

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ mongo_url = 'mongodb://localhost:27017/performance'
 db = Connection(MONGO_SERVER, 27017)['performance']
 
 # --------------------- object generation -------------------
-def make_string():
+def make_string(how_long):
     s = ''
-    for i in range(1, 100):
+    for i in range(1, how_long):
         s += chr((int)(random.random()*26) + 65)
     return s
 
@@ -22,21 +22,25 @@ def make_big_object(size):
         big_object[str(i)] = random.random() * 10000
 
     for i in range(1, size):
-        big_object['arrays ' + str(i)] = [random.random(), random.random(), random.random(), random.random(), random.random() ]
+        big_object['arrays ' + str(i)] = build_list(100)
 
     for i in range(1, size):
-        big_object['str ' + str(i)] = make_string()
+        big_object['str ' + str(i)] = make_string(100)
 
     return big_object
 
 def generate_objs(how_many, how_big):
     return [make_big_object(how_big) for i in range(0, how_many)]
 
-def generate_long_array_obj():
-    return {str(i) : build_list() for i in range(0, 10)}
+def generate_long_string_obj():
+    return {str(i) : make_string(100000) for i in range(0, 10)}
 
-def build_list():
-    return [(int)(random.random()*100000) for i in range(0, 10000)]
+
+def generate_long_array_obj():
+    return {str(i) : build_list(10000) for i in range(0, 10)}
+
+def build_list(how_long):
+    return [(int)(random.random()*100000) for i in range(0, how_long)]
 
 def randomize_10_cols(obj):
     for i in range(1, 11):
@@ -114,10 +118,10 @@ def analyze_inserts():
     time_it(insert_list,  {'obj_list':bos2, 'safe': True, 'collection_name': 'bos1'})
     time_it(insert_list,  {'obj_list':bos, 'safe': False, 'collection_name': 'bos2'})
 
-    massive = generate_objs(1, 10000)[0]
+    massive = generate_objs(1, 1000)[0]
     massive2 = copy.deepcopy(massive)
 
-    print ' Inserting 1 object with 30000 fields '
+    print ' Inserting 1 object with 3000 fields '
     time_it(insert,  {'obj': massive, 'safe': True, 'collection_name': 'mas1'})
     time_it(insert,  {'obj': massive2, 'safe': False, 'collection_name': 'mas2'})
     print '------------'
@@ -127,8 +131,8 @@ Conclusion: Update the whole object - dont try and update only the rows that cha
 that will take _much_ longer
 """
 def analyze_updates():
-    bos = generate_objs(1, 10000)[0]
-    print 'Inserting 1 objects with 30000 fields each'
+    bos = generate_objs(1, 1000)[0]
+    print 'Inserting 1 objects with 3000 fields each'
     print 'Next: update 10 of those fields'
 
     time_it(insert,  {'obj': bos, 'safe': True, 'collection_name': 'up'})
@@ -180,9 +184,9 @@ def analyze_reads_long_array():
     print '------------'
 
 """
-Conclusion: Updating only the column that changed can save time if arrays length > 1000.
+Conclusion: Updating only the column that changed can save time if arrays are long (length > 1000)
 If you save() the whole object then changing 1 field takes as long as rewriting all of the fields with different values.
-Updating 1 of the 10 giant arrays saves time compared to saving the whole object - On SSD ubuntu. Not on macbook pro.
+Updating 1 of the 10 giant arrays directly without calling save() saves time compared to saving the whole object
 """
 def analyze_updates_long_array():
     bos = generate_long_array_obj()
@@ -208,7 +212,7 @@ def analyze_updates_long_array():
     #bos = read(bos['_id'], 'up_l')
 
     print 'Change 1 column: full update'
-    bos['1'] = build_list()
+    bos['1'] = build_list(10000)
     time_it(update,  {'obj': bos, 'safe': True, 'collection_name': 'up_l'})
 
     print 'Change all columns: full update'
@@ -219,10 +223,27 @@ def analyze_updates_long_array():
     time_it(update,  {'obj': completly_new, 'safe': True, 'collection_name': 'up_l'})
 
     print 'Update 1 column at the DB level'
-    completly_new['1'] = build_list()
+    completly_new['1'] = build_list(10000)
     time_it(update_so_many_fields,  {'obj': completly_new, 'safe': True, 'how_many': 1, 'collection_name': 'up_l'})
 
     print '------------'
+
+
+"""
+(As expected) An object of long strings performs similarly to an object of long arrays
+"""
+def analyze_reads_long_strings():
+    bos = generate_long_string_obj()
+
+    print 'Inserting 1 objects with a few fields each field a long string'
+    print 'Next: read entire object or just ~ 1 fields of object'
+
+    time_it(insert,     {'obj': bos, 'safe': True, 'collection_name': 'read_l'})
+    time_it(read,       {'_id': bos['_id'], 'collection_name': 'read_l'})
+    time_it(read_1_col, {'_id' : bos['_id'], 'collection_name': 'read_l'})
+
+    print '------------'
+
 
 """
 (As expected) Doing this really will wipe out the old object - you will replace it with the
@@ -244,6 +265,8 @@ if __name__ == '__main__':
 
     analyze_reads_long_array()
     analyze_updates_long_array()
+
+    analyze_reads_long_strings()
 
     #analyze_partial_update()
     #cProfile.runctx('analyze_reads()', globals(), locals())
